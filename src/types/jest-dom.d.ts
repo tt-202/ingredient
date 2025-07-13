296e2f2ba54e453602c5139ab26d83dbb746da66
tt-202 <tuyentran02@yahoo.com>
2025-07-13 11:54:50 -0400

    fix: update API URLs to use Vercel domain

--- src/types/jest-dom.d.ts
+++
@@ -1,4 +0,0 @@
-import '@testing-library/jest-dom';
-
-// This file augments the Jest matchers to include testing-library's custom matchers
-// It resolves TypeScript errors for toBeInTheDocument() and other methods from @testing-library/jest-dom 
