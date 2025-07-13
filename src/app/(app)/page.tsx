2d0383bd85071adf5a3808cd69d4855f26a684e8
tt-202 <tuyentran02@yahoo.com>
2025-07-13 16:01:36 -0400

    Update page.tsx
    Text

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -2,10 +2,11 @@ export default function WelcomePage() {
   return (
     <div className="max-w-3xl mx-auto text-center">
       <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-        Welcome to Cooking Stories
+        Welcome to Ingredient Imposter
       </h1>
       <p className="mt-6 text-lg leading-8 text-gray-600">
-        Replace your ingredients.
+        Replace ingredients in recipes with other ingredients.
+      </p>
       </p>
     </div>
   );
